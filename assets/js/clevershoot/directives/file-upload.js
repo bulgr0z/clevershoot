d813d4dc721896bd4f87e03ec5cea34cb84dcd6d
Paul Sore <paul@MacBook-Pro-de-Paul.local>
2014-02-21 17:17:42 +0100

    fix file-upload / navbar

--- assets/js/clevershoot/directives/file-upload.js
+++ assets/js/clevershoot/directives/file-upload.js
@@ -10,7 +10,11 @@ clevershoot.directive('fileUpload', function() {
 				url: "/reference/"+scope.$parent.reference.id+"/image/upload",
 				success: function(dz, img) {
 					scope.image = img; // update scope
-					scope.$parent.reference.Images.unshift(img); // update scope
+					if (scope.$parent.reference.Images && scope.$parent.reference.Images.length) {
+						scope.$parent.reference.Images.unshift(img); // update scope
+					} else {
+						scope.$parent.reference.Images = [img]
+					}
 					scope.$apply();
 					$(element).find('.dz-preview').remove();
 				}
